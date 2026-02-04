5f6cfe8d6af47348ec07b38f609f6e9427614b78
nate127 <natecalhoun@seattleacademy.org>
2026-02-04 21:38:14 +0000

    hehehe

--- Projects/project4.py
+++ Projects/project4.py
@@ -2,7 +2,7 @@ import turtle, time, random
 from utils import *
 turtle.setup(width=964, height=540)
 # Section 1 - setup
-# TODO - set a background using set_background()
+#goal of the game is to get as much crypto as possible
 set_background("backk")
 # TODO - create at least "two variables and set their starting value. ex: cookies = 0
 mangos = 0
@@ -10,6 +10,8 @@ optiplex = 0
 cost = 50
 gpu = 0
 gpu_cost = 2000
+money_per_second = 0
+last_update_time = (time.time)
 # OPTIONAL: use this invisible alien to say a message
 message_sprite = create_sprite("alien", -435,140)
 message_sprite.hideturtle()
@@ -25,8 +27,8 @@ def get_gpu():
         gpu_cost = gpu_cost * 1.5
         gpu += 1
         x = -400 + 120 * gpu
-        y = 250
-        create_sprite("gpu1", x, y)
+        y = 0
+        create_sprite("card", x, y)
 def get_optiplex():
     global mangos, optiplex, cost
     if mangos >= cost:
@@ -43,7 +45,7 @@ window.onkeypress(get_mangos,"space")
 window.onkeypress(get_optiplex,"o")
 window.onkeypress(get_gpu,"g")
 mangos += optiplex
-mangos += gpu * 50
+mangos += gpu * 25
 # TODO - choose a key to do the action. ex: window.onkeypress(my_control, "space")
 
 # TODO - make a second control
@@ -55,16 +57,16 @@ mangos += gpu * 50
 # Section 3 - game loop
 window.listen()
 for i in range(1000000000):
-    message_sprite.color("red")
+    message_sprite.color("white")
     message_sprite.clear()
-    message_sprite.write(f"money: {mangos}\nCost: {cost}\noptiplex: {optiplex}\n gpu: {gpu}\ngpu_cost: {gpu_cost}", font=("",15,"bold"))
+    message_sprite.write(f"CRYPTO: {mangos}\nOptiplex Cost: {cost}\nOPTIPLEXES: {optiplex}\n GPUS: {gpu}\nGPU Cost: {gpu_cost}", font=("",15,"bold"))
 
     # TODO - put any automatic actions here
     mangos += optiplex
-    mangos += gpu * 50
+    mangos += gpu * 25
     # OPTIONAL - use the message sprite to say a message
     # message_sprite.clear()
     # message_sprite.write("MINE MY MANGO COIN FORVER OR ELSE")
 
-    time.sleep(0.1)
+    time.sleep(1)
     window.update()
