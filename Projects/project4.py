6411cd995047602a8293515e6707786b5b267597
nate127 <natecalhoun@seattleacademy.org>
2026-02-04 18:41:10 +0000

    project 4

--- Projects/project4.py
+++ Projects/project4.py
@@ -1,17 +1,17 @@
 import turtle, time, random
 from utils import *
-
+turtle.setup(width=964, height=540)
 # Section 1 - setup
 # TODO - set a background using set_background()
-set_background("background")
+set_background("backk")
 # TODO - create at least "two variables and set their starting value. ex: cookies = 0
 mangos = 0
 optiplex = 0
 cost = 50
 gpu = 0
-gpu_cost = 2500
+gpu_cost = 2000
 # OPTIONAL: use this invisible alien to say a message
-message_sprite = create_sprite("alien", -300,200)
+message_sprite = create_sprite("alien", -435,140)
 message_sprite.hideturtle()
 
 
@@ -19,14 +19,14 @@ message_sprite.hideturtle()
 # Section 2 - controls
 # TODO - define an action. ex: def my_control()
 def get_gpu():
-    global mangos, gpu, cost
+    global mangos, gpu, gpu_cost
     if mangos >= gpu_cost:
         mangos -= gpu_cost
-        cost = cost * 1.5
+        gpu_cost = gpu_cost * 1.5
         gpu += 1
         x = -400 + 120 * gpu
         y = 250
-        create_sprite("gpu", x, y)
+        create_sprite("gpu1", x, y)
 def get_optiplex():
     global mangos, optiplex, cost
     if mangos >= cost:
@@ -34,7 +34,7 @@ def get_optiplex():
         cost = cost * 2
         optiplex += 1
         x = -400 + 120 * optiplex
-        y = -250
+        y = -200
         create_sprite("image", x, y)
 def get_mangos():
     global mangos
@@ -43,6 +43,7 @@ window.onkeypress(get_mangos,"space")
 window.onkeypress(get_optiplex,"o")
 window.onkeypress(get_gpu,"g")
 mangos += optiplex
+mangos += gpu * 50
 # TODO - choose a key to do the action. ex: window.onkeypress(my_control, "space")
 
 # TODO - make a second control
@@ -54,13 +55,13 @@ mangos += optiplex
 # Section 3 - game loop
 window.listen()
 for i in range(1000000000):
-    message_sprite.color("green")
+    message_sprite.color("red")
     message_sprite.clear()
-    message_sprite.write(f"mangos: {mangos}\nCost: {cost}\noptiplex: {optiplex} gpu: {gpu}\ngpu_cost: {gpu_cost}", font=("arial",15,"bold"))
+    message_sprite.write(f"money: {mangos}\nCost: {cost}\noptiplex: {optiplex}\n gpu: {gpu}\ngpu_cost: {gpu_cost}", font=("",15,"bold"))
 
     # TODO - put any automatic actions here
     mangos += optiplex
-
+    mangos += gpu * 50
     # OPTIONAL - use the message sprite to say a message
     # message_sprite.clear()
     # message_sprite.write("MINE MY MANGO COIN FORVER OR ELSE")
